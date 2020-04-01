e4103c135d45a2042c94bd4a18859288b1227b1a
gyoomi <gyoomi0709@foxmail.com>
2020-04-01 18:31:55 +0800

    feat(xpath): xpath简单使用2

--- spider_start/xpath_test.py
+++ spider_start/xpath_test.py
@@ -3,5 +3,29 @@
 
 from scrapy import Selector
 
+html = """
+<html>
+    <body>
+        <h1>Hello World</h1>
+        <h1>Hello Scrapy</h1>
+        <h1>Hello python</h1>
+        <ul>
+            <li>C++</li>
+            <li>java</li>
+            <li>python</li>
+        </ul>
+    </body>
+    <h1>哈哈哈</h1>
+</html>
+"""
+
+selector = Selector(text=html)
+
+# 选中内容中的所有h1标签
+# data = selector.xpath("//h1")
+# print(data)
+
+data = selector.xpath("body/h1[4]")
+print(data)
 
 
