4842dc196583cf5464fc221758b00f48e2e8dbad
leon <gyoomi0709@foxmail.com>
2020-04-04 19:45:57 +0800

    feat(peewee): crud的使用

--- csdn_spider/peewee_test.py
+++ csdn_spider/peewee_test.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from peewee import *
+from datetime import date
 
 db = MySQLDatabase("spider", host="127.0.0.1", port=3306, user="root", password="root")
 
@@ -16,6 +17,30 @@ class Person(Model):
 		table_name = "user"
 
 
-if __name__ == "__main__":
-	db.create_tables([Person])
-	print("init [OK]")
+# if __name__ == "__main__":
+# 	db.create_tables([Person])
+# 	print("init [OK]")
+
+if __name__ == '__main__':
+
+	# 新增
+	# p = Person(name="jerry", birthday=date(1999, 2, 20))
+	# p.save()
+
+	# 查询
+	# 查询单个
+	# personInDb = Person.get_by_id(2)
+	# print(personInDb.name + ", " + str(personInDb.birthday))
+	# p = Person.select().where(Person.name == "tom").get()
+	# print(p.name + ", " + str(p.birthday))
+	# 查询列表
+	# personList = Person.select().where(Person.name == "jerry")
+	# for p in personList:
+	# 	print(p.name + ", " + str(p.birthday))
+
+	# 删除
+	# personList = Person.select().where(Person.name == "jerry")
+	# for p in personList:
+	# 	p.delete_instance()
+
+	print("ok")
