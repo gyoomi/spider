2a4759407eaddf26b98e21eb2718ae800e286aad
leon <gyoomi0709@foxmail.com>
2020-04-20 23:16:18 +0800

    feat(jd/xpath): 数据提起

---
+++ jd_spider/jd_selenium_spider.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import time
+import json
+
+from jd_spider.models import *
+
+
+from selenium import webdriver
+from scrapy import Selector
+
+browser = webdriver.Chrome(executable_path="D:/programs/dev/chromedriver_win32/chromedriver.exe")
+
+
+def parse_good(good_id):
+    browser.get("https://item.jd.com/{}.html".format(good_id))
+    sel = Selector(text=browser.page_source)
+
+    name = "".join(sel.xpath("//div[@class='sku-name']/text()").extract())
+    price = float("".join(sel.xpath("//span[@class='price J-p-{}']".format(good_id)).extract()).strip())
+    content = "".join(sel.xpath("//div[@id=''detail]//div[@class='tab-con']").extract())
+    image_list = sel.xpath("//div[@id='spec-list']/image/@src").extract()
+    supplier = "".join(sel.xpath("//div[@id='summary-service']/text()").extract())
+
+    comment_ele = browser.find_element_by_xpath("//li[@data-anchor='#comment']")
+    comment_ele.click()
+    time.sleep(4)
+
+    sel = Selector(text=browser.page_source)
+    good_rate = int(sel.xpath("//div[@class='percent-con']").extract()[0])
+    tag_list = sel.xpath("//div[@class='tag-list tag-available']//span/text()").extract()
+    comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|allpingjia']/@data-num").extract()[0]
+    has_image_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|shaidantab']/@data-num").extract()[0]
+    has_video_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|pingjiashipin']/@data-num").extract()[0]
+    has_add_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|zhuiping']/@data-num").extract()[0]
+    good_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|haoping']/@data-num").extract()[0]
+    middle_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|zhongping']/@data-num").extract()[0]
+    bad_comment_nums = sel.xpath("//ul[@class='filter-list']/li[@clstag='shangpin|keycount|product|chaping']/@data-num").extract()[0]
+
+    good = Good()
+    # TODO
+    pass
